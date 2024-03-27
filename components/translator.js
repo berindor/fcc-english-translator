ae1691770d80977bdbe62904ed60821a19c95ec8
Beringer Dorottya <beringerdorottya@gmail.com>
2024-03-27 11:12:44 +0100

    added tests and logic to translator.britToAm

--- components/translator.js
+++ components/translator.js
@@ -4,6 +4,19 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require('./american-to-british-titles.js');
 const britishOnly = require('./british-only.js');
 
+const wordsGrouped = (arr, groupSize, start) => {
+  let arrOfGroups = [];
+  //push words separately before start
+  for (let i = 0; i < start; i++) {
+    arrOfGroups.push(arr[i]);
+  }
+  for (let j = start; j < arr.length; j += groupSize) {
+    let wordsToJoin = arr.slice(j, j + groupSize);
+    arrOfGroups.push(wordsToJoin.join(' '));
+  }
+  return arrOfGroups;
+};
+
 class Translator {
   amToBrit(inputString) {
     //This does not work:
@@ -15,7 +28,7 @@ class Translator {
       let translation;
       if (americanToBritishTitles[stringToTranslate] !== undefined) {
         translation = americanToBritishTitles[stringToTranslate];
-      } else if (/\.$/.test(wordString)) {
+      } else if (/[\.,?]$/.test(wordString)) {
         stringToTranslate = wordString.toLowerCase().slice(0, -1);
       }
       if (americanToBritishSpelling[stringToTranslate] !== undefined) {
@@ -36,22 +49,9 @@ class Translator {
         translatedWord = translation[0].toUpperCase() + translation.slice(1);
       }
       if (stringToTranslate.length < wordString.length) {
-        return translatedWord + '.';
+        return translatedWord + wordString.slice(-1);
       }
       return translatedWord;
-    };
-
-    const wordsGrouped = (arr, groupSize, start) => {
-      let arrOfGroups = [];
-      //push words separately before start
-      for (let i = 0; i < start; i++) {
-        arrOfGroups.push(arr[i]);
-      }
-      for (let j = start; j < arr.length; j += groupSize) {
-        let wordsToJoin = arr.slice(j, j + groupSize);
-        arrOfGroups.push(wordsToJoin.join(' '));
-      }
-      return arrOfGroups;
     };
 
     let previousSentence;
@@ -73,11 +73,52 @@ class Translator {
   }
 
   britToAm(inputString) {
-    const numOfWords = Object.keys(americanOnly).map(key => key.split(' ').length);
-    //console.log(`numOfWords: ${numOfWords}`);
+    //const numOfWords = Object.keys(britishOnly).map(key => key.split(' ').length);
     //console.log(`longest multiple word is ${Math.max(...numOfWords)} long`);
+    const findTranslationToAm = wordString => {
+      let stringToTranslate = wordString.toLowerCase();
+      let translation = Object.keys(americanToBritishSpelling).find(key => americanToBritishSpelling[key] === stringToTranslate);
+      if (americanToBritishTitles[stringToTranslate + '.'] !== undefined) {
+        translation = stringToTranslate + '.';
+      } else if (/[\.,?]$/.test(wordString)) {
+        stringToTranslate = wordString.toLowerCase().slice(0, -1);
+      }
+      if (britishOnly[stringToTranslate] !== undefined) {
+        translation = britishOnly[stringToTranslate];
+      }
+      if (/^[0-9]{1,2}\.[0-9]{2}$/.test(stringToTranslate)) {
+        const indexOfDot = stringToTranslate.search(/\./);
+        translation = stringToTranslate.slice(0, indexOfDot) + ':' + stringToTranslate.slice(indexOfDot + 1, stringToTranslate.length);
+      }
 
-    return inputString;
+      if (translation === undefined) return wordString;
+
+      let translatedWord = translation;
+      if (wordString[0] === wordString[0].toUpperCase()) {
+        translatedWord = translation[0].toUpperCase() + translation.slice(1);
+      }
+      if (stringToTranslate.length < wordString.length) {
+        return translatedWord + wordString.slice(-1);
+      }
+      return translatedWord;
+    };
+
+    let previousSentence;
+    let sentence = inputString;
+    let arrOfTerms;
+    //all terms in britishOnly consists of 1, 2 or 3 words
+    for (let groupSize = 3; groupSize > 0; groupSize--) {
+      for (let start = 0; start < groupSize; start++) {
+        do {
+          previousSentence = sentence;
+          arrOfTerms = wordsGrouped(previousSentence.split(/\s+/), groupSize, start);
+          sentence = arrOfTerms.map(term => findTranslationToAm(term)).join(' ');
+          //length of terms can change in this steps --> iteration needed
+        } while (sentence !== previousSentence);
+      }
+    }
+
+    return sentence;
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,7 @@ const Translator = require('../components/translator.js');
 let { amToBrit, britToAm } = new Translator();
 
 suite('Unit Tests', () => {
-  suite.only('Translate American to British English', () => {
+  suite('Translate American to British English', () => {
     test(`Mangoes are my favorite fruit. => ...favourite...`, () => {
       const amString = 'Mangoes are my favorite fruit.';
       const britString = 'Mangoes are my favourite fruit.';
@@ -58,55 +58,55 @@ suite('Unit Tests', () => {
     });
   });
   suite('Translate British to American English', () => {
-    test(`We watched the footie match for a while. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`We watched the footie match for a while. => ...soccer...`, () => {
+      const britString = `We watched the footie match for a while.`;
+      const amString = `We watched the soccer match for a while.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`Paracetamol takes up to an hour to work. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`Paracetamol takes up to an hour to work. => Tylenol...`, () => {
+      const britString = `Paracetamol takes up to an hour to work.`;
+      const amString = `Tylenol takes up to an hour to work.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`First, caramelise the onions. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`First, caramelise the onions. => ...caramelize...`, () => {
+      const britString = `First, caramelise the onions.`;
+      const amString = `First, caramelize the onions.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`I spent the bank holiday at the funfair. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`I spent the bank holiday at the funfair. => ...public holiday...carnival.`, () => {
+      const britString = `I spent the bank holiday at the funfair.`;
+      const amString = `I spent the public holiday at the carnival.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`I had a bicky then went to the chippy.  => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`I had a bicky then went to the chippy.  => ...cookie...fish-and-chip shop.`, () => {
+      const britString = `I had a bicky then went to the chippy.`;
+      const amString = `I had a cookie then went to the fish-and-chip shop.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`I've just got bits and bobs in my bum bag. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`I've just got bits and bobs in my bum bag. => ...odds and ends...fanny pack.`, () => {
+      const britString = `I've just got bits and bobs in my bum bag.`;
+      const amString = `I've just got odds and ends in my fanny pack.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`The car boot sale at Boxted Airfield was called off. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`The car boot sale at Boxted Airfield was called off. => ...swap meet...`, () => {
+      const britString = `The car boot sale at Boxted Airfield was called off.`;
+      const amString = `The swap meet at Boxted Airfield was called off.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`Have you met Mrs Kalyani? => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`Have you met Mrs Kalyani? => ...Mrs. ...`, () => {
+      const britString = `Have you met Mrs Kalyani?`;
+      const amString = `Have you met Mrs. Kalyani?`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`Prof Joyner of King's College, London. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`Prof Joyner of King's College, London. => Prof. ...`, () => {
+      const britString = `Prof Joyner of King's College, London.`;
+      const amString = `Prof. Joyner of King's College, London.`;
+      assert.equal(britToAm(britString), amString);
     });
-    test(`Tea time is usually around 4 or 4.30. => `, () => {
-      const britString = ``;
-      const amString = ``;
-      assert.equal(britToAm(amString), britString);
+    test(`Tea time is usually around 4 or 4.30. => ...4:30`, () => {
+      const britString = `Tea time is usually around 4 or 4.30.`;
+      const amString = `Tea time is usually around 4 or 4:30.`;
+      assert.equal(britToAm(britString), amString);
     });
   });
   suite('Highlighting', () => {
